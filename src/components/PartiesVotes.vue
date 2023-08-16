d7865a6088b1d4a24e501b07addefa9caf509515
sanavalos <santiago.avalos@outlook.com>
2023-08-15 21:34:14 -0300

    refactor: correct componentization

---
+++ src/components/PartiesVotes.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { getPrimariesData } from '../services/services.js'
+
+const primariesData = ref([])
+
+function setPrimariesData() {
+  getPrimariesData()
+    .then((response) => {
+      primariesData.value = response.data.partidos
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+onMounted(() => {
+  setPrimariesData()
+})
+</script>
+
+<template>
+  <main>
+    <ul>
+      <li v-for="primary in primariesData" :key="primary.code">
+        {{ primary.name }} - {{ primary.votos }}
+      </li>
+    </ul>
+  </main>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,9 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import { getPrimariesData } from '../services/services.js'
-
-const primariesData = ref([])
-
-function setPrimariesData() {
-  getPrimariesData()
-    .then((response) => {
-      console.log('response', response.data)
-      primariesData.value = response.data.partidos
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
-onMounted(() => {
-  setPrimariesData()
-})
+import PartiesVotes from '../components/PartiesVotes.vue'
 </script>
 
 <template>
   <main>
-    <ul>
-      <li v-for="primary in primariesData" :key="primary.code">
-        {{ primary.name }} - {{ primary.votos }}
-      </li>
-    </ul>
+    <PartiesVotes/>
   </main>
 </template>
