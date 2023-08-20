093b26bfb2334e50f870f43a6a3cfad814b6d208
sanavalos <santiago.avalos@outlook.com>
2023-08-20 17:34:06 -0300

    feat: add landing view

---
+++ src/views/LandingView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import RederictButtons from '../components/RederictButtons.vue'
+</script>
+
+<template>
+  <main>
+    <h1>
+      Unveil the Pulse of Democracy: Discover PASO Primaries Results for Argentina's Elections!
+    </h1>
+    <RederictButtons />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
