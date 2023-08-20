8f206f3c5751c983acd393fd11644a3349e34d9d
sanavalos <santiago.avalos@outlook.com>
2023-08-20 17:34:14 -0300

    feat: add not found view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import RederictButtons from '../components/RederictButtons.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Page not found</h1>
+    <RederictButtons />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
