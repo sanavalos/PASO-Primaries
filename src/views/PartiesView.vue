0e231c95c7d38861646875b20b50f83e5133323a
sanavalos <santiago.avalos@outlook.com>
2023-08-20 17:38:03 -0300

    refactor: create parties and votes view

---
+++ src/views/PartiesView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderOptions from '../components/HeaderOptions.vue'
+import Parties from '../components/Parties.vue'
+</script>
+
+<template>
+  <main>
+    <HeaderOptions />
+    <Parties />
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 70vw;
+}
+</style>
---
+++ src/views/VotesView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderOptions from '../components/HeaderOptions.vue'
+import Votes from '../components/Votes.vue'
+</script>
+
+<template>
+  <main>
+    <HeaderOptions />
+    <Votes />
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 70vw;
+}
+</style>
